f69f47d84dbb7623742ea7dd3d1edc3a59694ff3
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2020-10-02 21:01:17 -0300

    classe Seller no entities

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
 import db.DB;
+import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.Connection;
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        Department dep = new Department(15, "Computadores");
+        Seller sel = new Seller(16, "Silas", "silas@gmail.com", new Date(), 2000.0, dep);
+
+        System.out.println(sel);
     }
 }
