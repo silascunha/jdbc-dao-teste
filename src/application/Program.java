89dadf1f614aa7e09ef71e6cb2c3fc6c1f4d26b0
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2020-10-02 22:08:47 -0300

    DaoFactory e SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
 import db.DB;
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
     public static void main(String[] args) {
         Department dep = new Department(15, "Computadores");
         Seller sel = new Seller(16, "Silas", "silas@gmail.com", new Date(), 2000.0, dep);
+
+        SellerDAO sellerDAO = DaoFactory.createSellerDao();
 
         System.out.println(sel);
     }
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.dao.impl;
+
+import model.dao.SellerDAO;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDAO {
+
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void update(Seller obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
